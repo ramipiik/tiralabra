8df28fe6635d60247d312ece6a3523acd5db2266
Rami Piik <rami.piik@gmail.com>
2021-10-10 18:10:11 +0300

    linting recap.py

--- Main/Start/recap.py
+++ Main/Start/recap.py
@@ -1,13 +1,14 @@
-# Recaps the choices made by the user
+"""Recaps the choices made by the user"""
 
 
 def recap(players, board_size, level, first_move):
+    """Recaps the choices made by the user"""
     print("Awesome! Let's recap your choices.")
     print("----------------")
     print("Players:", players)
     print("Board size:", str(board_size) + "x" + str(board_size))
-    if players == 0 or players == 1:
-        if level == 1 or level == "1":
+    if players in (0, 1):
+        if level in (1, "1"):
             print("Level of AI: 1 (Easy)")
         if level == 2:
             print("Level of AI: 2 (Medium)")
