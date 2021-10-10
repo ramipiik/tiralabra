43ca041231aaba207a4c518da2f08205c73ccc5c
Rami Piik <rami.piik@gmail.com>
2021-10-10 18:35:21 +0300

    linting boundaries_check.py

--- Main/Heuristics/boundaries_check.py
+++ Main/Heuristics/boundaries_check.py
@@ -1,13 +1,14 @@
+"""Calculates the distance of the marks from the boundaries"""
 from tictactoe import TicTacToe
-
-# Calculates the distance of the marks from the boundaries. Center positions are valued higher than edge positions
-# Input parameters:
-# tictactoe: state of the board
-# mark: X or O
-# first_time: If computer goes second, encourages computer to put its mark so that it's connected to opponent's mark
 
 
 def boundaries_check(tictactoe: TicTacToe, mark: str):
+    """Calculates the distance from edges. Center positions are valued higher than edge positions"""
+    # Input parameters:
+    # tictactoe: state of the board
+    # mark: X or O
+    # first_time: If computer goes second, steers it put its mark connected to opponent's mark
+
     distance = 0
     table = []
     for i in range(tictactoe.board_size):
