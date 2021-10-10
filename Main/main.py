df7d5494e0c87b335ebca3ec0dbcf0637f9daa03
Rami Piik <rami.piik@gmail.com>
2021-10-10 15:22:24 +0300

    taking pylint into use

--- Main/main.py
+++ Main/main.py
@@ -1,20 +1,26 @@
+"""
+Asks for user inputted parameters and starts the game.
+"""
+
+import datetime
 from tictactoe import TicTacToe
 from play import play
 from Start.start import start
-import datetime
 
-# Starts the program by asking for user inputs. After that starts the game.
 def main():
+    """Asks for user inputted parameters and starts the game."""
     user_choices = start()
     players = user_choices[0]
     board_size = user_choices[1]
     level = user_choices[2]
     first_move = user_choices[3]
 
-    if first_move == 2:
-        x_starts = True
-    else:
-        x_starts = False
+    # if first_move == 2:
+    #     x_starts = True
+    # else:
+    #     x_starts = False
+
+    x_starts = (first_move == 2)
 
     custom_board = (board_size ** 2) * "-"
     state = TicTacToe(custom_board, x_starts, level, players, True)
