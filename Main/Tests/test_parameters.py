e13fad26f60ef22a45bee7c5de832c3a76609a47
Rami Piik <rami.piik@gmail.com>
2021-10-10 20:02:42 +0300

    linting test_parameters.py

--- Main/Tests/test_parameters.py
+++ Main/Tests/test_parameters.py
@@ -1,24 +1,25 @@
+"""Tests the parameters module"""
 # Execute the test class by running the following comman from command line:
 # python3 -m unittest -v Tests.Test_parameters
 
+import unittest
 from parameters import how_much_to_win, closeness_weight, LARGE_NUMBER
 from tictactoe import TicTacToe
-import unittest
-from unittest.mock import patch
 
 
-class Test_parameters_class(unittest.TestCase):
+
+class TestParameters(unittest.TestCase):
     def test_how_much_to_win(self):
-        self.assertEquals(how_much_to_win(3), 3)
-        self.assertEquals(how_much_to_win(5), 4)
-        self.assertEquals(how_much_to_win(7), 4)
-        self.assertEquals(how_much_to_win(10), 5)
-        self.assertEquals(how_much_to_win(15), 5)
-        self.assertEquals(how_much_to_win(20), 5)
-        self.assertEquals(how_much_to_win(100), 5)
-        self.assertEquals(how_much_to_win(0), 5)
-        self.assertEquals(how_much_to_win(-1), 5)
-        self.assertEquals(how_much_to_win("-"), 5)
+        self.assertEqual(how_much_to_win(3), 3)
+        self.assertEqual(how_much_to_win(5), 4)
+        self.assertEqual(how_much_to_win(7), 4)
+        self.assertEqual(how_much_to_win(10), 5)
+        self.assertEqual(how_much_to_win(15), 5)
+        self.assertEqual(how_much_to_win(20), 5)
+        self.assertEqual(how_much_to_win(100), 5)
+        self.assertEqual(how_much_to_win(0), 5)
+        self.assertEqual(how_much_to_win(-1), 5)
+        self.assertEqual(how_much_to_win("-"), 5)
 
         with self.assertRaises(TypeError):
             how_much_to_win()
@@ -29,22 +30,22 @@ class Test_parameters_class(unittest.TestCase):
     def test_get_max_depth(self):
         board = "XXX------"
         node = TicTacToe(board, False, 1, 2)
-        self.assertEquals(node.get_max_depth(), 0)
+        self.assertEqual(node.get_max_depth(), 0)
 
         node = TicTacToe(board, False, 2, 2)
-        self.assertEquals(node.get_max_depth(), LARGE_NUMBER)
+        self.assertEqual(node.get_max_depth(), LARGE_NUMBER)
 
         board = "-------------------------"
         node = TicTacToe(board, False, 2, 2)
-        self.assertEquals(node.get_max_depth(), 4)
+        self.assertEqual(node.get_max_depth(), 4)
 
         board = "------------------------------"
         node = TicTacToe(board, False, 2, 2)
-        self.assertEquals(node.get_max_depth(), 3)
+        self.assertEqual(node.get_max_depth(), 3)
 
         board = "------------------------------------------------------------"
         node = TicTacToe(board, False, 2, 2)
-        self.assertEquals(node.get_max_depth(), 2)
+        self.assertEqual(node.get_max_depth(), 2)
 
         with self.assertRaises(TypeError):
             node.get_max_depth(1)
@@ -53,11 +54,11 @@ class Test_parameters_class(unittest.TestCase):
             node.get_max_depth(3, 4)
 
     def test_closeness_weight(self):
-        self.assertEquals(closeness_weight(2), 0.25)
-        self.assertEquals(closeness_weight(1), 0)
-        self.assertEquals(closeness_weight(0), 0)
-        self.assertEquals(closeness_weight(-1), 0)
-        self.assertEquals(closeness_weight("-"), 0)
+        self.assertEqual(closeness_weight(2), 0.25)
+        self.assertEqual(closeness_weight(1), 0)
+        self.assertEqual(closeness_weight(0), 0)
+        self.assertEqual(closeness_weight(-1), 0)
+        self.assertEqual(closeness_weight("-"), 0)
 
         with self.assertRaises(TypeError):
             closeness_weight()
--- Main/run_all_tests.sh
+++ Main/run_all_tests.sh
@@ -5,8 +5,8 @@ python3 -m unittest Tests.Heuristics.Test_closeness_check -b
 python3 -m unittest Tests.Heuristics.Test_boundaries_check -b
 python3 -m unittest Tests.Heuristics.Test_basic_check -b
 python3 -m unittest Tests.test_alphabeta -b
-python3 -m unittest Tests.Test_start -b
-python3 -m unittest Tests.Test_parameters -b
-python3 -m unittest Tests.Test_play -b
-python3 -m unittest Tests.Test_tictactoe -b
+python3 -m unittest Tests.test_start -b
+python3 -m unittest Tests.test_parameters -b
+python3 -m unittest Tests.test_play -b
+python3 -m unittest Tests.test_tictactoe -b
 
