21331b0fb4adb3aa988e3a3e29a4d4db34b69ddb
Rami Piik <rami.piik@gmail.com>
2021-10-10 18:25:30 +0300

    linting basic_check.py

--- Main/Heuristics/basic_check.py
+++ Main/Heuristics/basic_check.py
@@ -1,90 +1,42 @@
+"""Gives a score to the board based on the combinations found"""
 from Heuristics.helper import line_checker
 from tictactoe import TicTacToe
 
-# Gives a score to the board based on the combinations found
-# Combination is expressed as a tuple where first parameter contains the string/combination and second parameter its value
-# Input parameters:
-# tictactoe: state of the board
-# mark: X or O
-# n: How many in a row are required to win
 
-
-def basic_check(tictactoe: TicTacToe, mark: str, n: int):
+def basic_check(tictactoe: TicTacToe, mark: str, n_to_win: int):
+    """Gives a score to the board based on the combinations found"""
+    # Combination is a tuple. 1st parameter is the string/combination. 2nd parameter is its value
 
     combos = []
-    combos_2 = []
-    combos_3 = []
-    combos_4 = []
 
-    if n == 2:
-        combo = mark + "-" + mark
-        combos_2.append((combo, 1))
-        combo = "-" + mark + mark
-        combos_2.append((combo, 1))
-        combo = mark + mark + "-"
-        combos_2.append((combo, 1))
+    if n_to_win == 2:
+        combos.append((mark + "-" + mark, 1))
+        combos.append(("-" + mark + mark, 1))
+        combos.append((mark + mark + "-", 1))
 
-    if n == 3:
-        combo = mark + "-" + mark
-        combos_3.append((combo, 1))
-        combo = "-" + mark + mark
-        combos_3.append((combo, 1))
-        combo = mark + mark + "-"
-        combos_3.append((combo, 1))
+    if n_to_win == 3:
+        combos.append((mark + "-" + mark, 1))
+        combos.append(("-" + mark + mark, 1))
+        combos.append((mark + mark + "-", 1))
+        combos.append((mark + "-" + mark + mark, 5))
+        combos.append((mark + "-" + mark + mark, 5))
+        combos.append(("-" + mark + mark + mark, 7))
+        combos.append((mark + mark + mark + "-", 7))
 
-        combo = mark + "-" + mark + mark
-        combos_3.append((combo, 5))
-        combo = mark + mark + "-" + mark
-        combos_3.append((combo, 5))
+    if n_to_win == 4:
+        combos.append((mark + "-" + mark, 1))
+        combos.append(("-" + mark + mark, 1))
+        combos.append((mark + mark + "-", 1))
+        combos.append((mark + "-" + mark + mark, 3))
+        combos.append((mark + mark + "-" + mark, 3))
+        combos.append(("-" + mark + mark + mark, 7))
+        combos.append((mark + mark + mark + "-", 7))
+        combos.append((mark + "-" + mark + mark + mark, 15))
+        combos.append((mark + mark + "-" + mark + mark, 15))
+        combos.append((mark + mark + mark + "-" + mark, 15))
+        combos.append(("-" + mark + mark + mark + mark, 20))
+        combos.append((mark + mark + mark + mark + "-", 20))
 
-        combo = "-" + mark + mark + mark
-        combos_3.append((combo, 7))
-
-        combo = mark + mark + mark + "-"
-        combos_3.append((combo, 7))
-
-    if n == 4:
-
-        combo = mark + "-" + mark
-        combos_4.append((combo, 1))
-        combo = "-" + mark + mark
-        combos_4.append((combo, 1))
-        combo = mark + mark + "-"
-        combos_4.append((combo, 1))
-
-        combo = mark + "-" + mark + mark
-        combos_4.append((combo, 3))
-        combo = mark + mark + "-" + mark
-        combos_4.append((combo, 3))
-
-        combo = "-" + mark + mark + mark
-        combos_4.append((combo, 7))
-
-        combo = mark + mark + mark + "-"
-        combos_4.append((combo, 7))
-
-        combo = mark + "-" + mark + mark + mark
-        combos_4.append((combo, 15))
-
-        combo = mark + mark + "-" + mark + mark
-        combos_4.append((combo, 15))
-
-        combo = mark + mark + mark + "-" + mark
-        combos_4.append((combo, 15))
-
-        combo = "-" + mark + mark + mark + mark
-        combos_4.append((combo, 20))
-
-        combo = mark + mark + mark + mark + "-"
-        combos_4.append((combo, 20))
-
-    if n == 2:
-        combos = combos_2
-    if n == 3:
-        combos = combos_3
-    if n == 4:
-        combos = combos_4
-
-    count = line_checker(combos, tictactoe, n)
+    count = line_checker(combos, tictactoe, n_to_win)
 
     return count
